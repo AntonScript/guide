e5a062a86399bcf4db5c2385861e258e50cc637e
Anton Mamakin <antonmamakinscript@gmail.com>
2021-06-28 11:07:33 +0400

    исправлена модель dto user

--- src/main/java/team/mediasoft/guide/controller/dto/UserResponseDto.java
+++ src/main/java/team/mediasoft/guide/controller/dto/UserResponseDto.java
@@ -1,17 +1,19 @@
 package team.mediasoft.guide.controller.dto;
 
+import java.util.Set;
+
 public class UserResponseDto {
     private Long id;
     private String login;
-    private Long idRole;
+    private Set<String> roles;
 
     public UserResponseDto() {
     }
 
-    public UserResponseDto(Long id, String login, Long idRole) {
+    public UserResponseDto(Long id, String login, Set<String> roles) {
         this.id = id;
         this.login = login;
-        this.idRole = idRole;
+        this.roles = roles;
     }
 
     public Long getId() {
@@ -22,7 +24,7 @@ public class UserResponseDto {
         return login;
     }
 
-    public Long getIdRole() {
-        return idRole;
+    public Set<String> getRoles() {
+        return roles;
     }
 }
